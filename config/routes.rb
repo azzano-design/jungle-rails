e9a8eaa6f06c435458ba70f1041ff74c34f68cb1
Brendan <brendanarchangelo@gmail.com>
2018-01-18 23:52:51 +0000

    Created authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,17 @@ Rails.application.routes.draw do
     resources :products, :categories, except: [:edit, :update, :show]
   end
 
+  # Sessions
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # Users
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  resources :users
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
